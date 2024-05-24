c9869e0e50708f1e6ccb03ace633e13baa130dd4
mc <mcapsan@gmail.com>
2024-05-24 03:28:37 -0400

    Add function getComputerChoice to generate random choices

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-console.log("Hello, World!");
-alert("Hello, World!");
+function getComputerChoice() {
+	let choice = "";
+	let randomNum = Math.floor(Math.random() * 3);
+	if (randomNum === 0) {
+		choice = "Rock";
+	} else if (randomNum === 1) {
+		choice = "Paper";
+	} else {
+		choice = "Scissors";
+	}
+	return choice
+}
+
+
