00656f479c38a1ef487dab1625660c6aea7ada43
mc <mcapsan@gmail.com>
2024-05-24 03:53:24 -0400

    Move playRound into playGame and play 5 rounds, announcing a winner in the end

--- script.js
+++ script.js
@@ -12,52 +12,64 @@ function getHumanChoice() {
 	}
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
 
-function playRound(humanChoice, computerChoice) {
-	switch (humanChoice) {
-		case 1:
-			if (computerChoice === 2) {
-				computerScore++;
-				console.log("You lose! Paper beats Rock!");
-			} else if (computerChoice === 3) {
-				humanScore++;
-				console.log("You win! Rock beats Scissors!");
-			} else {
-				console.log("Draw! You both picked Rock!");
-			}
-			break;
-		case 2:
-			if (computerChoice === 1) {
-				humanScore++;
-				console.log("You win! Paper beats Rock!");
-			} else if (computerChoice === 3) {
-				computerScore++;
-				console.log("You lose! Scissors beats Paper!");
-			} else {
-				console.log("Draw! You both picked Paper!");
-			}
-			break;
-		case 3:
-			if (computerChoice === 1) {
-				computerScore++;
-				console.log("You lose! Rock beats Scissors!");
-			} else if (computerChoice === 2) {
-				humanScore++;
-				console.log("You win! Scissors beats Paper!");
-			} else {
-				console.log("Draw! You both picked Scissors!");
-			}
-			break;
-		default:
-			break;
+	let humanScore = 0;
+	let computerScore = 0;
 
+	function playRound(humanChoice, computerChoice) {
+		switch (humanChoice) {
+			case 1:
+				if (computerChoice === 2) {
+					computerScore++;
+					console.log("You lose! Paper beats Rock!");
+				} else if (computerChoice === 3) {
+					humanScore++;
+					console.log("You win! Rock beats Scissors!");
+				} else {
+					console.log("Draw! You both picked Rock!");
+				}
+				break;
+			case 2:
+				if (computerChoice === 1) {
+					humanScore++;
+					console.log("You win! Paper beats Rock!");
+				} else if (computerChoice === 3) {
+					computerScore++;
+					console.log("You lose! Scissors beats Paper!");
+				} else {
+					console.log("Draw! You both picked Paper!");
+				}
+				break;
+			case 3:
+				if (computerChoice === 1) {
+					computerScore++;
+					console.log("You lose! Rock beats Scissors!");
+				} else if (computerChoice === 2) {
+					humanScore++;
+					console.log("You win! Scissors beats Paper!");
+				} else {
+					console.log("Draw! You both picked Scissors!");
+				}
+				break;
+			default:
+				break;
+
+		}
+	}
+
+	for (let i = 0; i < 5; i++) {
+		playRound(getHumanChoice(), getComputerChoice());
+	}
+
+	if (humanScore > computerScore) {
+		console.log("Yay! You won!!!");
+	} else if (humanScore < computerScore) {
+		console.log("Oh no! You lost!");
+	} else {
+		console.log("Draw! How did this happen? We played an odd number of rounds");
 	}
 }
 
-let hChoice = getHumanChoice();
-let cChoice = getComputerChoice();
-
-playRound(hChoice, cChoice);
+playGame()
 
